3e163921c73177a60029139444dcea7def0f6e6c
Harvey Rogers <harveyr@gmail.com>
2019-11-14 09:17:08 -0800

    check run

--- src/main.ts
+++ src/main.ts
@@ -1,101 +1,23 @@
 import * as core from '@actions/core'
-import { Lint, runEslint, getEslintVersion, parseEslints } from './eslint'
-
 import * as kit from '@harveyr/github-actions-kit'
-
-// TODO: Use a TS import once this is fixed: https://github.com/actions/toolkit/issues/199
-// import * as github from '@actions/github'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const github = require('@actions/github')
-
-const GITHUB_WORKSPACE = kit.getWorkspace()
+import { getEslintVersion, parseEslints, runEslint } from './eslint'
+import { getAnnotationForLint } from './annotations'
+import { Lint } from './types'
 
 // It appears the setup-node step adds a "problem matcher" that will catch lints
 // and create annotations automatically!
 const POST_ANNOTATIONS = false
 
-function getAnnotationLevel(severity: string): kit.AnnotationLevel {
-  if (severity === 'error') {
-    return 'failure'
-  }
-  // not sure what the actual string is yet
-  if (severity.indexOf('warn') === 0) {
-    return 'warning'
-  }
-  return 'notice'
-}
+async function postCheckRun(lints: Lint[]): Promise<void> {
+  const annotations = lints.map(getAnnotationForLint)
 
-function getAnnotationForLint(lint: Lint): kit.CheckRunAnnotation {
-  const { filePath, line, message, severity } = lint
-  const path = filePath.substring(GITHUB_WORKSPACE.length + 1)
-  return {
-    path,
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    startLine: line,
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    level: getAnnotationLevel(severity),
-    message,
-  }
-}
-
-async function postAnnotations(lints: Lint[]): Promise<void> {
-  if (!GITHUB_WORKSPACE) {
-    return core.setFailed(
-      'GITHUB_WORKSPACE not set. This should happen automatically.',
-    )
-  }
-  if (!GITHUB_REPOSITORY) {
-    return core.setFailed('GITHUB_REPOSITORY was not set')
-  }
-  if (!GITHUB_SHA) {
-    return core.setFailed('GITHUB_SHA was not set')
-  }
-
-  const annotations = []
-  for (const lint of lints) {
-    const { filePath, line, message, severity } = lint
-    const path = filePath.substring(GITHUB_WORKSPACE.length + 1)
-    annotations.push({
-      path,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      start_line: line,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      end_line: line,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      annotation_level: getAnnotationLevel(severity),
-      message,
-    })
-  }
-
-  const repoData = github.context.payload.repository
-  if (!repoData) {
-    return core.setFailed('repository not found')
-  }
-
-  const [owner, repo] = GITHUB_REPOSITORY.split('/')
-  core.debug(`Found Github owner ${owner}, repo ${repo}`)
-
-  const githubToken = core.getInput('github-token')
-  if (!githubToken) {
-    return core.setFailed('github-token is required')
-  }
-
-  const client = new github.GitHub(githubToken)
-
-  console.log(`Posting ${annotations.length} annotations`)
-
-  await client.checks.create({
+  await kit.postCheckRun({
+    githubToken: core.getInput('github-token'),
     name: 'ESLint',
     conclusion: annotations.length ? 'failure' : 'success',
     // eslint-disable-next-line @typescript-eslint/camelcase
-    head_sha: GITHUB_SHA,
-    owner,
-    repo,
-    output: {
-      title: 'ESLint',
-      summary: `${annotations.length} lints reported`,
-      annotations,
-    },
+    summary: `${annotations.length} lints reported`,
+    annotations: POST_ANNOTATIONS ? annotations : [],
   })
 }
 
@@ -117,12 +39,7 @@ async function run(): Promise<void> {
 
   const output = await runEslint(patterns, { cwd })
   const lints = parseEslints(output)
-  if (POST_ANNOTATIONS) {
-    await postAnnotations(lints)
-  }
-  if (lints.length) {
-    core.setFailed(`ESLint found ${lints.length} issues`)
-  }
+  await postCheckRun(lints)
 }
 
 run().catch(err => {
