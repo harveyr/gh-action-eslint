e5d1f26fff33ffb128fb25b1e8f5907789ea322c
Harvey Rogers <harveyr@gmail.com>
2019-11-15 15:54:07 -0800

    wip

--- src/annotations.ts
+++ src/annotations.ts
@@ -4,11 +4,12 @@ import { Lint } from './types'
 const GITHUB_WORKSPACE = kit.getWorkspace()
 
 function getAnnotationLevel(severity: string): kit.AnnotationLevel {
+  severity = severity.toLowerCase()
   if (severity === 'error') {
     return 'failure'
   }
   // not sure what the actual string is yet
-  if (severity.indexOf('warn') === 0) {
+  if (severity === 'warning') {
     return 'warning'
   }
   return 'notice'
@@ -19,9 +20,7 @@ export function getAnnotationForLint(lint: Lint): kit.CheckRunAnnotation {
   const path = filePath.substring(GITHUB_WORKSPACE.length + 1)
   return {
     path,
-    // eslint-disable-next-line @typescript-eslint/camelcase
     startLine: line,
-    // eslint-disable-next-line @typescript-eslint/camelcase
     level: getAnnotationLevel(severity),
     message,
   }
